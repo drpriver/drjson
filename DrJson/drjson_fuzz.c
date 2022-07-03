8f7d6d1101edd62499a5f2282ef959ae3f4b0c03
drpriver <david@davidpriver.com>
2022-07-02 21:21:21 -0700

    add fuzzing

---
+++ DrJson/drjson_fuzz.c
@@ -0,0 +1,14 @@
+#define DRJSON_API static inline
+#include "drjson.c"
+
+// Libfuzz calls this function to do its fuzzing.
+
+int
+LLVMFuzzerTestOneInput(const uint8_t* data, size_t size){
+    DrJsonAllocator allocator = drjson_stdc_allocator();
+    DrJsonValue result = drjson_parse_string(allocator, (const char*)data, size);
+    if(result.kind != DRJSON_ERROR){
+        drjson_slow_recursive_free_all(&allocator, result);
+    }
+    return 0;
+}
