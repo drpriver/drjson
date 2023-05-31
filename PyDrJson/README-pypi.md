f2f6b9f484c757cb5e1989756b03dbb01daace71
drpriver <david@davidpriver.com>
2023-05-30 22:32:56 -0700

    add readme and type annots

---
+++ PyDrJson/README-pypi.md
@@ -0,0 +1,3 @@
+# drjson
+This is a module for fast json parsing. Idk if it's actually faster than other fast json parsing libs.
+But it also offers some nice extensions on top of json.
---
+++ PyDrJson/drjson/drjson.pyi
@@ -0,0 +1,80 @@
+from typing import Tuple, Any, Protocol, Callable, overload, Final
+version: Tuple[int, int, int]
+__version__: str
+ERROR: int
+NUMBER: int
+INTEGER: int
+UINTEGER: int
+STRING: int
+ARRAY: int
+OBJECT: int
+NULL: int
+BOOL: int
+ARRAY_VIEW: int
+OBJECT_KEYS: int
+OBJECT_VALUES: int
+OBJECT_ITEMS: int
+APPEND_NEWLINE: int
+PRETTY_PRINT: int
+
+class Ctx:
+    def parse(self, text:str, braceless:bool=False) -> Value:
+        ...
+
+    def make(self, value: Any) -> Value:
+        ...
+
+class Writer(Protocol):
+    def write(self, s:str) -> None:
+        ...
+
+class Value:
+    ctx: Ctx
+    kind: Final[int]
+
+    def __repr__(self) -> str:
+        ...
+
+    def __getitem__(self, k:Union[int, str]) -> Value:
+        ...
+
+    def __delitem__(self, k:Union[int, str]) -> None:
+        ...
+
+    def __setitem__(self, k:Union[int, str], v:Any) -> None:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+    def py(self) -> Union[int, str, float, list, dict]:
+        ...
+
+    def query(self, query:str, type:int=None) -> Value:
+        ...
+
+    def clear(self) -> None:
+        ...
+
+    # only valid if kind == ARRAY
+    def append(self, item:Any) -> None:
+        ...
+
+    # only valid if kind == ARRAY
+    def pop(self) -> Value:
+        ...
+
+    # only valid if kind == ARRAY
+    def insert(self, whence:int, item:Any) -> None:
+        ...
+
+    @overload
+    def dump(self, writer:None=None, flags:int=0) -> str:
+        ...
+
+    @overload
+    def dump(self, writer:Union[Writer, Callable[[str], None]], flags:int=0) -> None:
+        ...
+
+    def dump(self, writer:Union[None, Writer, Callable[[str], None]]=None, flags:int=0) -> Union[str, None]:
+        ...
